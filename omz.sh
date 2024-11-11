a6a14431546f4eedd6161166c6be04acea6615d6
Shubs <shubs@sqprojekt.com>
2024-11-11 02:50:45 -0500

    refactor: improved checking if OMZ is installed

--- omz.sh
+++ omz.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
-# Check for Oh My Zsh and install if we don't have it
-if ! test $(which omz); then
+# Check if Oh My Zsh is installed by looking for the .oh-my-zsh directory
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is not installed. Installing now..."
     /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+else
+    echo "Oh My Zsh is already installed."
 fi
-
-echo "Oh my zsh installation done."
